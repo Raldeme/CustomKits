f6323e303af568dd9425ca6501538f868876f7cd
Raldeme <nixxous@gmail.com>
2021-12-22 01:06:03 +0000

    Update AssemblyInfo.cs

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -5,7 +5,7 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Kits")]
+[assembly: AssemblyTitle("CustomKits")]
 [assembly: AssemblyDescription("A Rocket Mod plugin for the Steam game Unturned, allowing players to store items on a server for later access.")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
